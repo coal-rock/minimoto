17702b5b1fd9c99943f0e8415e94c120270d0e07
Rueben Engelhart <engelhartrueben@gmail.com>
2026-04-25 14:00:00 -0400

    menu continuation w/ button rect support

--- src/menu.py
+++ src/menu.py
@@ -3,6 +3,12 @@ import pygame as pg
 from button import Button
 from helper import *
 
+START_BTN_LOC = (50, 150)
+
+SETTINGS_BTN_LOC = (75, 250)
+
+EXIT_BTN_LOC = (75, 350)
+
 class Menu:
     __screen: pg.Surface
 
@@ -13,14 +19,48 @@ class Menu:
     def __init__(self, screen: pg.Surface):
         self.__screen = screen
 
-    def init_start_btn(self) -> Button:
-        pass
+        self.__start_btn = Button(
+                START_BTN_LOC[0], 
+                START_BTN_LOC[1], 
+                self.__screen, 
+                self.test,
+                rect_mode=True,
+                rect_h=50,
+                rect_w=150)
 
-    def init_settings_btn(self) -> Button:
-        pass
+        self.__settings_btn = Button(
+                SETTINGS_BTN_LOC[0], 
+                SETTINGS_BTN_LOC[1], 
+                self.__screen, 
+                self.test,
+                rect_mode=True,
+                rect_h=50,
+                rect_w=100)
 
-    def init_exit_btn(self) -> Button:
-        pass
+        self.__exit_btn = Button(
+                EXIT_BTN_LOC[0], 
+                EXIT_BTN_LOC[1], 
+                self.__screen, 
+                self.exit_btn_onclick,
+                rect_mode=True,
+                rect_h=50,
+                rect_w=100)
+
+    def test(self):
+        print("TEST")
 
     def exit_btn_onclick(self):
         exit_game()
+
+    def click(self, x: int, y: int):
+        self.__start_btn.click_if(x, y)
+        self.__settings_btn.click_if(x, y)
+        self.__exit_btn.click_if(x, y)
+
+    def update(self, dt: float):
+        pass
+
+    def draw(self):
+        self.__start_btn.draw()
+        self.__settings_btn.draw()
+        self.__exit_btn.draw()
