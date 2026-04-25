d06a626704dd1680bb29bb605581feedaacf77a0
SoftwareByThomasKita <thomaskita526@gmail.com>
2026-04-25 12:38:14 -0400

    Map update w/ car reference

---
+++ assets/tiled/car_reference.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="car_reference" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="../car/car000.png" width="100" height="100"/>
+</tileset>
